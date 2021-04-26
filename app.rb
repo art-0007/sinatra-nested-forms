0252cf0c7081f7584841718bbaf32570c545c99d
Artur <info@aigroupusa.com>
2021-04-26 16:35:30 -0400

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,26 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do 
+      #binding.p'ry
+      erb :root
+    end
+
+    get '/new'
+    erb :'pirates/new'
+    end
+
+    # post '/pirates'
+    # binding.pry
+    #   @pirate = Pirate.new(params[:pirate])
+    #   params["pirate"]["ships"].each do |ship|
+    #     Ship.new(ship)
+    #   end
+    #   @ship = Ship.all
+
+    #   erb :'pirates/show'
+    # end
   end
-end
+
+  end
+
